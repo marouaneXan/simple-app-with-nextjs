d7e817aa1622759a621de3943b058b88dca865db
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-04 11:59:38 +0100

    display all articles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.articles)
   return (
     <div>
       <h1>Hello world</h1>
     </div>
   )
+}
+export const getStaticProps=async() =>{
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+  return {
+    props:{
+      articles
+    }
+  }
 }
