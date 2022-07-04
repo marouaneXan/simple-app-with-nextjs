22a45f64e4983abf331cf4a27f2114e87f1eddb3
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-04 11:06:09 +0100

    add header component

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>
+            <span>
+                WebDev 
+            </span>News
+        </h1>
+        <style>
+            
+        </style>
+    </div>
+  )
+}
+
+export default Header
