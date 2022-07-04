313d59236d71aa16a4917469f6300120bd12ce6f
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-04 11:30:26 +0100

    some changes in Layouts component

--- components/Header.js
+++ components/Header.js
@@ -3,19 +3,12 @@ import headerStyles from '../styles/Header.module.css'
 const Header = () => {
   return (
     <div>
-        <h1 className='title'>
+        <h1 className={headerStyles.title}>
             <span>
                 WebDev 
             </span>News
         </h1>
-        <style jsx>
-            {`
-            .title{
-                color:red;
-
-            }
-            `}
-        </style>
+        <p className={headerStyles.description}>keep up to date with the latest web dev news</p>
     </div>
   )
 }
