bd9532cb4712d3a606e4f2280d45372003de3487
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-04 14:42:21 +0100

    fetch details of article

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,12 +1,25 @@
-import {useRouter} from 'next/router'
-const article = () => {
-    const router = useRouter()
-    const {id} = router.query
+import Link from 'next/link'
+const article = ({article}) => {
+    console.log(article)
   return (
     <div>
-       This is article {id}
+       This is article {article.id}
+       <h1>{article.title}</h1>
+       <p>{article.body}</p>
+       <br/>
+       <Link href="/">Go Back</Link>
     </div>
   )
 }
-
+export const getServerSideProps=async (context)=>{
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+      );
+      const article = await res.json();
+      return {
+        props: {
+          article,
+        },
+      };
+}
 export default article
